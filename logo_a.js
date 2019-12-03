5c95530b04cd2eebfb4aa32ee22b1e23287b512c
dorlibrary <58445143+dorlibrary@users.noreply.github.com>
2019-12-03 13:20:14 -0800

    Update logo_a.js

--- logo_a.js
+++ logo_a.js
@@ -1,1 +1,5 @@
 document.getElementById('logo_a').appendChild(document.createTextNode(new Date().getFullYear()))
+jQuery(document).ready(function() {
+     jQuery("#logo_a:eq( 2 )").children().first().html('<img src="https://raw.githubusercontent.com/dorlibrary/hosting/master/KP-logo-white1.png"     style="padding-bottom: 5px; background:#003c71">');
+});
+
