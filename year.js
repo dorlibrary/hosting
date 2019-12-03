f35033e5aede624ec8c268bc64cae8cbb7d50077
dorlibrary <58445143+dorlibrary@users.noreply.github.com>
2019-12-03 12:40:59 -0800

    Update year.js

--- year.js
+++ year.js
@@ -1,5 +1,3 @@
-(function() {
-  document.getElementById('year').appendChild(document.createTextNode(new Date().getFullYear()))
-}
-
-})();
+jQuery(document).ready(function() {
+  jQuery(".logo_a:eq( 2 )").children().first().html('<img src="https://raw.githubusercontent.com/dorlibrary/hosting/master/KP-logo-white1.png"     style="padding-bottom: 5px; background:#003c71">');
+});
