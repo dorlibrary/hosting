1a60e7cf5175adaf6b55f077cb01f7f8375a9f26
dorlibrary <58445143+dorlibrary@users.noreply.github.com>
2019-12-03 13:52:16 -0800

    Update logo_a.js

--- logo_a.js
+++ logo_a.js
@@ -1,3 +1,7 @@
 var logo = document.getElementById('logoa');
 logo.setAttribute("href", "?SS_Page=refiner&meduid=<?pmid?>&doi=<?DOI?>&genre=<?genre?>&atitle=<?atitle?>&title=<?title?>&client=s6295818&issn=<?issn?>&isbn=<?isbn?>&volume=<?volume?>&issue=<?issue?>&date=<?date?>&aulast=<?aulast?>");
 logo.setAttribute("text", "MySQL Tutorial");
+
+jQuery(document).ready(function() {
+     jQuery("#logoa:eq( 2 )").children().first().html('<img src="https://raw.githubusercontent.com/dorlibrary/hosting/master/KP-logo-white1.png" style="padding-bottom: 5px; background:#003c71">');
+});
