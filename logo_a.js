ba92c9531a3f0452c7f228e713c9b426b61c116e
dorlibrary <58445143+dorlibrary@users.noreply.github.com>
2019-12-03 13:12:19 -0800

    Update logo_a.js

--- logo_a.js
+++ logo_a.js
@@ -1,3 +1,1 @@
-jQuery(document).ready(function() {
-  jQuery(".logo_a:eq( 2 )").children().first().html('<span>text</span>');
-});
+document.getElementsByClassName("logo_a").value = "Johnny Bravo";
