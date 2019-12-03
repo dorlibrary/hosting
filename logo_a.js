7588d2b81901ead7245e7b500f7a850d98bfb21a
dorlibrary <58445143+dorlibrary@users.noreply.github.com>
2019-12-03 13:23:21 -0800

    Update logo_a.js

--- logo_a.js
+++ logo_a.js
@@ -3,3 +3,6 @@ jQuery(document).ready(function() {
      jQuery("#logo_a:eq( 2 )").children().first().html('<img src="https://raw.githubusercontent.com/dorlibrary/hosting/master/KP-logo-white1.png"     style="padding-bottom: 5px; background:#003c71">');
 });
 
+var logo = document.getElementById('logo_a');
+logo.createElement('a');
+logo.setAttribute('href', 'javascript:logo_a()');
