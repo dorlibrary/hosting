f7fb206b7d79fc48f430d58c7a3516eec620df47
dorlibrary <58445143+dorlibrary@users.noreply.github.com>
2019-12-03 13:49:45 -0800

    Update logo_a.js

--- logo_a.js
+++ logo_a.js
@@ -1,3 +1,3 @@
 var logo = document.getElementById('logoa');
-logo.setAttribute("href", "www.w3resource.com/mysql/mysql-tutorials.php");
+logo.setAttribute("href", "?SS_Page=refiner&meduid=<?pmid?>&doi=<?DOI?>&genre=<?genre?>&atitle=<?atitle?>&title=<?title?>&client=s6295818&issn=<?issn?>&isbn=<?isbn?>&volume=<?volume?>&issue=<?issue?>&date=<?date?>&aulast=<?aulast?>");
 logo.setAttribute("text", "MySQL Tutorial");
